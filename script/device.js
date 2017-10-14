5b615675103d7b4834aa7d7443ec8b99c5517a60
guodong <cody.guo@soulinfo.com>
2017-10-14 17:26:49 +0800

    完成基础版本

--- script/device.js
+++ script/device.js
@@ -1,37 +1,370 @@
-(function(window){
+(function(window) {
 
     window.stDevice = {
+        /**
+         *  ret
+            {
+              status://【0：设备OK， 1：需要更新设备信息 ，2：需要定标, -2:设备连接失败 -1：没有设备】
+            }
 
-          isDeviceOk : function(callback) {
-              if (!isLoad){
-                  loadAppConfig();
-              }
-              var ret = {};
-              ret.status = false;
-              if (appConfig.device) {
-                    ret.status = true;
-                    ret.device = appConfig.device;
-              }
-              if (typeof callback === "function"){
-                  callback(ret);
-              }
-          },
+         */
+        isDeviceOk: function(callback) {
+            callback = callback || function(ret) {
+                console.log(JSON.stringify(ret));
+            };
+            if (!isLoad) {
+                loadAppConfig();
+            }
+            var ret = {};
+            ret.status = 0;
+            if (appConfig.device) {
+                ret.device = appConfig.device;
+                sppUtil.connectDevice(appConfig.device.address, function(connectRet) {
+                    if (connectRet.err) {
+                        console.log(connectRet.err);
+                        ret.status = -2;
+                        ret.err = connectRet.err
+                    } else {
+                        if (!ret.device.model) {
+                            ret.status = 1;
+                        } else if (!ret.device.darkCurrent || !ret.device.whiteboardData) {
+                            ret.status = 2;
+                        }
+                    }
+                    callback(ret);
+                });
+            } else {
+                ret.status = -1;
+                callback(ret);
+            }
+        },
 
-          addDevice : function(device){
-                if (!isLoad){
-                    loadAppConfig();
+        readDeviceInfo : function(address, callback){
+            hyCmd.receive(address,  {
+                batteryInfo : function(data){
+                  if (typeof callback == "function")
+                    callback(data);
+                },
+                deviceInfo : function(data){
+                  var model = data.substr(0, 4);
+                  var serial = data.substr(4, 8);
+                  var firmware = data.substr(12, 4);
+                  if (appConfig.device) {
+                      appConfig.device.model = model;
+                      appConfig.device.serial = serial;
+                      appConfig.device.firmware = firmware;
+                      getDCRecord(address, function(ret){
+                          if (ret.status && ret.dc && ret.dc.length == 2){
+                              appConfig.device.darkCurrent = ret.dc[0];
+                              appConfig.device.whiteboardData = ret.dc[1];
+                          }
+                          saveAppConfig();
+                          loadAppConfig();//读取全局配置信息
+                          hyCmd.batteryInfo(address);
+                      });
+                    }
                 }
-                window.appConfig.device = device;
-                saveAppConfig();
-          },
-
-          removeDevice : function(){
-              if (!isLoad){
-                  loadAppConfig();
-              }
-              window.appConfig.device = null;
-              saveAppConfig();
-          }
+            });
+            hyCmd.deviceInfo(address);
+        }
     }
+
+    function getDCRecord(address, callback){
+        api.readFile({
+            path: 'fs://dc/' + address
+        }, function(ret, err){
+            var funcRet = {};
+            if( ret.status ){
+                 var dc = JSON.parse(ret.data);
+                 funcRet.status = true;
+                 funcRet.dc = dc;
+                 if (typeof callback == "function")
+                    callback(funcRet);
+            } else {
+              funcRet.status = true;
+              funcRet.err = err;
+              if (typeof callback == "function")
+                 callback(funcRet);
+            }
+        });
+
+    }
+
+    var __hyCmd = {
+
+        collect: function(address, callback) {
+            var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090010013B0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        collectOne: function(address, callback) {
+          var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090010023C0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        stopCollect: function(address, callback) {
+          var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090010003A0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        periodSetup: function(address, data,callback) {
+          if (data > 255) data = 255;
+          if (data < 1) data = 1;
+          var period = data.toString(16);
+          var base = 315//13B 55 + cc + 9 + 11;
+          var crc = (base + data).toString(16);
+          if (crc.length > 2){
+              crc = crc.substring(crc.length - 2);
+          }
+          var sendData = "55CC090011" + period + crc + "0000";
+          console.log(sendData.toUpperCase());
+          var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: sendData.toUpperCase(),
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        lightOn: function(address, callback) {
+          var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090012114D0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        lightOff: function(address, callback) {
+          var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090012104C0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        deviceInfo: function(address, callback) {
+            spp = spp || api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090020004A0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        },
+
+        batteryInfo: function(address, callback) {
+          var spp = api.require("spp");
+            spp.send({
+                address: address,
+                isHex: true,
+                sendData: '55CC090021004B0000',
+            }, function(ret) {
+                console.log(JSON.stringify(ret));
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            })
+        }
+
+    }
+
+    __hyCmd.receive = function(address, handler) {
+        var spp = api.require("spp");
+        var remainData = "";
+        spp.receive({
+            address: address,
+            isHex: true
+        }, function(ret) {
+            if (ret.data) {
+                var data = remainData + ret.data.toLocaleUpperCase();
+                var cmds = parseCmd(data);
+                if (cmds.length == 2) {
+                    cmdHandler(cmds[0], handler);
+                    remainData = cmds[1];
+                } else {
+                    remainData = cmds[0];
+                }
+            } else {
+                console.log(JSON.stringify(ret));
+            }
+
+        });
+    }
+
+    function parseCmd(data) {
+        var cmds = [];
+        var beginIndex = data.indexOf('55CC');
+        if (beginIndex < 0) {
+            cmds.push(data);
+        } else {
+            var lengthL = data.substr(beginIndex + 4, 2);
+            var lengthH = data.substr(beginIndex + 6, 2);
+            var dataLength = parseInt(lengthH + lengthL, 16);
+            var beforeCmd = data.substring(0, data.indexOf('55CC') - 1);
+            var afterCmd = data.substring(data.indexOf('55CC'));
+            var leftStr = null;
+            var cmd = null;
+            if (afterCmd.length >= dataLength * 2) {
+                cmd = afterCmd.substr(0, dataLength * 2);
+                console.log("cmd返回开始:" + cmd.substr(0, 8));
+                console.log("cmd返回长度:" + beginIndex + ":" + lengthH + ":" + lengthL + ":" + dataLength);
+                console.log("cmd返回结束:" + cmd.substr(cmd.length - 4));
+                var reg = /(^55CC)[0123456789ABCDEF]+(0000$)/i;
+                leftStr = afterCmd.substr(dataLength * 2);
+                if (reg.test(cmd)) {
+                    cmds.push(cmd);
+                    cmds.push(leftStr);
+                } else {
+                    console.log("=====cmd返回无效======");
+                    cmds.push(leftStr);
+                }
+            } else {
+                cmds.push(afterCmd);
+            }
+        }
+        return cmds;
+    }
+
+    function cmdHandler(cmd, handler) {
+        handler = handler || {};
+        var remain = "";
+        if (cmd.length < 18) {
+            remain = cmd;
+        } else if (cmd.indexOf('55CC') == 0 && cmd.substring(cmd.length - 4) == "0000") {
+            var lengthL = cmd.substr(4, 2);
+            var lengthH = cmd.substr(6, 2);
+            var dataLength = parseInt(lengthH + lengthL, 16);
+            var cmdType = null;
+            var data = null;
+            if (cmd.length != dataLength * 2) {
+                console.log("数据长度不正确:记录长度[" + dataLength + "],实际长度[" + cmd.length + "]");
+                remain = cmd;
+            } else {
+                cmdType = cmd.substr(8, 2);
+                data = cmd.substr(10, cmd.length - 16);
+                if (cmdType == "10") {
+                    doWithCollect(data, handler.collect, handler.stopCollect);
+                } else if (cmdType == "11") {
+                    returnFormPeriodSetup(data, handler.periodSetup);
+                } else if (cmdType == "12") {
+                    returnLightSwitch(data, handler.lightSwitch);
+                } else if (cmdType == "20") {
+                    returnModelInfo(data, handler.deviceInfo);
+                } else if (cmdType == "21") {
+                    returnBatteryInfo(data, handler.batteryInfo);
+                } else {
+                    console.log("不支持的命令");
+                }
+            }
+        } else {
+            remain = cmd;
+            console.log("无效的数据返回:" + cmd);
+        }
+        return remain;
+    }
+
+    function doWithCollect(data, handler, stopHandler) {
+        handler = handler || function() {};
+        stopHandler = stopHandler || function() {};
+        console.log("采集数据:" + data.length);
+        if (data.length == 2) {
+            stopHandler();
+            return;
+        }
+        var range = appConfig.device.spectralRange;
+        var labels = [],
+            datas = [],
+            hexData = [];
+        var index = 0;
+        for (var i = 0; i < data.length; i++) {
+            var pos = i % 4;
+            if (pos == 0) {
+                var numStr = data[i + 2] + data[i + 3] + data[i] + data[i + 1];
+                hexData[index++] = parseInt(numStr, 16);
+            }
+        }
+        for (var i = range[0]; i <= range[1]; i++) {
+            labels.push(1.9799 * i - 934.5831);
+            datas.push(hexData[i]);
+        }
+        handler(datas, labels);
+    }
+
+    function returnFormPeriodSetup(data, handler) {
+        console.log("曝光时间:" + data);
+        handler = handler || function() {};
+        handler(data);
+    }
+
+    function returnLightSwitch(data, handler) {
+        console.log("开关灯光:" + data);
+        handler = handler || function() {};
+        handler(data);
+    }
+
+    function returnModelInfo(data, handler) {
+        console.log("型号信息:" + data);
+        handler = handler || function() {};
+        handler(data);
+    }
+
+    function returnBatteryInfo(data, handler) {
+        console.log("电池电量:" + data);
+        handler = handler || function() {};
+        var d = parseInt(data, 16);
+        appConfig.device.batteryInfo = d;
+        saveAppConfig();
+        handler(data);
+    }
+
+    window.hyCmd = __hyCmd;
 
 })(window);
--- script/spp.js
+++ script/spp.js
@@ -1,168 +1,127 @@
-(function(window){
-  var __bluetooth ;
+(function(window) {
+    var __bluetooth;
+    var spp;
 
-  window.bleInit = function(callback) {
-    window.SPP = api.require("btsppiot");
-    SPP.INIT({
-        //isToast:true
-    },function(ret, err){
-        var status = ret.status;
-        if(status == 1)api.toast({ msg: "本机没有蓝牙设备"});
-        if(status == 2)api.toast({ msg: "本机蓝牙没有开启"});
+    var __sppUtil = {};
 
-        __bluetooth = __bleUtil.loadConfig();
+    __sppUtil.init = function(opts, callback) {
+        spp = api.require('spp');
 
-        __bluetooth.state = ret.state;
-        __bluetooth.show = __bluetooth.show || {};
-        if (typeof callback === "function"){
-            window.bluetooth = __bluetooth;
-            callback(__bluetooth);
+        if (typeof opts == "function") {
+            callback = opts;
+            opts = {};
         }
-    });
+        opts = opts || {}
+        var open = opts.open || false;
 
-    SPP.autoConnect();
-  }
+        var me = this;
 
-  /**
-   * 扫描设备
-   * @param autoStop 自动停止扫描时间 单位second
-   */
-  window.scanDevice = function(autoStop, foundFunc){
-      SPP.scan({
-          isDialog:false,
-          //isToast:false
-      },function(ret, err){
-          console.log(JSON.stringify(ret));
-          var status = ret.status;
-        if(status == 1)api.toast({ msg: "本机没有蓝牙设备"});
-        if(status == 2)api.toast({ msg: "本机蓝牙没有开启"});
-        var bondState = ret.bondState;
-        if(bondState == 1)api.toast({ msg: "取消配对"});
-        if(bondState == 2)api.toast({ msg: "已经配对"});
-        if(bondState == 3)api.toast({ msg: "正在配对"});
-        var scanState = ret.scanState;
-        if(scanState == 1)api.toast({ msg: "扫描开始"});
-        if(scanState == 2)api.toast({ msg: "扫描完成"});
-        if(scanState == 3)api.toast({ msg: "发现设备"});
-        if(scanState == 4)api.toast({ msg: "配对改变"});
-        var scanArray = ret.scanArray;
-        if (scanArray){
-          var deviceStrArray = scanArray.split(',');
-         for(var i in deviceStrArray){
-             var dev = {};
-             var deviceStr = deviceStrArray[i];
-             if (deviceStr.indexOf('[') == 0)
-               deviceStr = deviceStr.substring(1);
-             if (deviceStr.indexOf(']') == deviceStr.length - 1)
-               deviceStr = deviceStr.substring(0, deviceStr.length - 1);
-             var infoArray = deviceStr.split('\n');
-             console.log("name:" + infoArray[0]);
-             console.log("uuid:" + infoArray[1]);
-             dev.uuid = infoArray[1];
-             dev.name = infoArray[0];
-             if (typeof foundFunc == "function")
-               foundFunc(dev);
-             }
-        }
-
-         if(scanArray.length != 0)
-        console.log(scanArray);
-      });
-  }
-
-
-  var devicesListener = null;
-
-  /**
-   *更新扫描设备
-   *@param foundFunc 发现新设备的回调
-   */
-  function updateDevices(foundFunc){
-      var newDevices = {};
-     devicesListener = setInterval(function () {
-        ble.getPeripheral(function(ret) {
-          if (ret && ret.peripherals) {
-              var i;
-              for(i = 0; i < ret.peripherals.length; i++){
-                var dev = ret.peripherals[i];
-                if (!newDevices[dev.uuid]){
-                    console.log("发现新设备：" + dev.uuid);
-                    newDevices[dev.uuid] = dev;
-                    if (typeof foundFunc == "function")
-                      foundFunc(dev);
-                }
-              }
-          }
+        __bluetooth = {};
+        spp.init({
+            open: open
+        }, function(ret) {
+            __bluetooth.status = ret.status;
+            __bluetooth.show = {};
+            switch (ret.status) {
+                case 'poweredOn':
+                    __bluetooth.show.status = "设备已开启";
+                    break;
+                case 'poweredOff':
+                    __bluetooth.show.status = "设备已关闭";
+                    break;
+                case 'resetting':
+                    __bluetooth.show.status = "正在重置";
+                    break;
+                case 'unauthorized':
+                    __bluetooth.show.status = "设备未授权";
+                    break;
+                case 'unknown':
+                    __bluetooth.show.status = "初始化中";
+                    break;
+                case 'unsupported':
+                    __bluetooth.show.status = "状态不支持";
+                    break;
+                default:
+                    break;
+            }
+            if (typeof callback === "function") {
+                callback(__bluetooth);
+            }
         });
-      }, 1000);
-  }
+    }
 
-  /**
-   *停止扫描设备
-   */
-  window.stopScanDevice = function () {
-      console.log("停止扫描蓝牙设备");
-    //  SPP.paired();
-  }
+    /**
+     * 扫描设备
+     * @param autoStop 自动停止扫描时间 单位second
+     */
+    __sppUtil.scanDevice = function(autoStop, foundFunc) {
+        console.log("开始扫描蓝牙设备");
+        spp.scan({}, function(ret, err) {
+            if (ret.status == "BLUTTOOTH_DISABLED") {
+                api.alert({
+                    title: '错误',
+                    msg: '蓝牙未启用，请启用蓝牙设备',
+                });
+            } else if (ret.status == "FOUND") {
+                console.log("发现新设备：" + ret.device);
+                var device = JSON.parse(ret.device);
+                if (typeof foundFunc == "function")
+                    foundFunc(device);
+            } else if (ret.status == "DISCOVERY_FINISHED") {
+                console.log("扫描完成，共发现" + ret.devicesNum + "个设备");
+            } else if (ret.status == "DISCOVERY_STARTED") {
+                console.log("开始扫描");
+            }
+        });
+        if (autoStop > 0)
+            setTimeout(__sppUtil.stopScanDevice, 1000 * autoStop);
+    }
 
-  /**
-   * 断开连接
-   * @param uuid 设备uuid
-   * @param callback 回调
-   */
-  function disconnectDevice(uuid, callback) {
-      ble.disconnect({
-          peripheralUUID: uuid
-      }, function(ret, err) {
-          console.log(JSON.stringify(err));
-          if (ret.status) {
-              api.toast({
-                  msg: '断开连接成功',
-                  duration: 2000,
-                  location: 'top'
-              });
-              bluetooth.device = null;
-              bleUtil.saveConfig(bluetooth);
-          }
-          if (typeof callback === "function"){
-              callback(ret);
-          }
-      });
-  }
+    /**
+     *停止扫描设备
+     */
+    __sppUtil.stopScanDevice = function() {
+        console.log("停止扫描蓝牙设备");
+        spp.stopScan();
+    }
 
-  /**
-   *连接设备
-   */
-  window.connectDevice = function(uuid){
-    SPP.connect({
-      sppMac:uuid
-    }, function(ret, err){
-        console.log(JSON.stringify(ret));
-    });
-    SPP.autoConnect();
-  }
+    /**
+     * 断开连接
+     * @param address 设备地址
+     * @param callback 回调
+     */
+    __sppUtil.disconnectDevice = function(address, callback) {
+        spp.disconnect({
+            address: address
+        }, function(ret) {
+            if (typeof callback === "function") {
+                callback(ret);
+            }
+        });
+    }
 
-  window.sendData = function(data, callback){
-      SPP.send({
-        sendData:"55CC090010013B0000"
-      });
-  }
+    /**
+     *连接设备
+     * @param address 设备地址
+     * @param callback 回调
+     */
+    __sppUtil.connectDevice = function(address, callback) {
+        spp.connect({
+            address: address
+        }, function(ret, err) {
+            console.log(JSON.stringify(ret));
+            if (ret.err) {
+                if (typeof callback == "function") {
+                    callback(ret);
+                }
+            } else if (ret.device) {
+                if (typeof callback == "function") {
+                    callback(JSON.parse(ret.device));
+                }
+            }
+        });
+    }
 
-
-  var __bleUtil = {};
-
-  __bleUtil.loadConfig = function(){
-      var bluet = $api.getStorage('bluetooth');
-      if (bluet == null) {
-          bluet = {};
-          bluet.show = {};
-      }
-      return bluet;
-  }
-
-  __bleUtil.saveConfig = function(bluet){
-      $api.setStorage('bluetooth', bluet);
-  }
-  window.bleUtil = __bleUtil;
-
+    window.sppUtil = __sppUtil;
+    window.spp = spp;
 })(window);
